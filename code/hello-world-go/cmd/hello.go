d2214918edeb2e451f15280ed6c81cc7896d247e
Håkon Åmdal <hakon@aamdal.com>
2021-07-30 12:49:50 +0100

    proper error handling with wrap

--- code/hello-world-go/cmd/hello.go
+++ code/hello-world-go/cmd/hello.go
@@ -5,14 +5,20 @@ import (
 	"os"
 
 	argparse "github.com/akamensky/argparse"
+	"github.com/hawkaa/experimenting/code/hello-world-go/internal"
 )
 
 func main() {
 	parser := argparse.NewParser("hello", "Hello Something-er")
-	s := parser.String("n", "name", &argparse.Options{Required: true, Help: "Who do you want to say hello to?"})
+	s := parser.Float("s", "seed", &argparse.Options{Required: true, Help: "Seed number"})
 	if err := parser.Parse(os.Args); err != nil {
 		fmt.Print(parser.Usage(err))
 		return
 	}
-	fmt.Printf("Hello %s!\n", *s)
+	magic, err := internal.DoMagic(*s)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+	fmt.Printf("%v\n", magic)
 }
--- code/hello-world-go/go.mod
+++ code/hello-world-go/go.mod
@@ -2,4 +2,7 @@ module github.com/hawkaa/experimenting/code/hello-world-go
 
 go 1.16
 
-require github.com/akamensky/argparse v1.3.0
+require (
+	github.com/akamensky/argparse v1.3.0
+	github.com/pkg/errors v0.9.1
+)
